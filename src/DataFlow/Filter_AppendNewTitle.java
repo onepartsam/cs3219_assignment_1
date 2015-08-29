81140134094f8a7ac1edc628089696b7e3da1cdf
Bay Chuan Wei Candiie <Candiiebay@gmail.com>
2015-08-30 02:59:41 +0800

    Create and Implemented the AppendNewTitle Filter which inherits the Filter base class.

---
+++ src/DataFlow/Filter_AppendNewTitle.java
@@ -0,0 +1,30 @@
+package DataFlow;
+
+import java.util.LinkedList;
+
+public class Filter_AppendNewTitle extends Filter {
+
+	//Class Variables & Objects
+	private String newTitle;
+	private LinkedList<String> kwicIndex;
+	
+	Filter_AppendNewTitle(Pipe inPipe) {
+		super(inPipe);
+	}
+	
+	@Override
+	public void processData() {
+		
+		newTitle = pipe.getStrValue();
+		kwicIndex = pipe.getLinkedListValue();
+		kwicIndex.add(newTitle);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public LinkedList<String> getProcessedData() {
+
+		return kwicIndex;
+	}
+	
+};
