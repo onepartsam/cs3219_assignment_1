6988e24a2b82a362753c1b20e78d9affbf516c4d
Bay Chuan Wei Candiie <Candiiebay@gmail.com>
2015-08-30 02:58:44 +0800

    Create and Implement the CheckKeyword Filter which inherits the filter class, this class basically checks if the first word of the sentence is a word to ignore or a keyword, if its a word to ignore getProcessedData will return false since it is not a keyword , otherwise it will return true since it is a keyword..

---
+++ src/DataFlow/Filter_CheckKeyword.java
@@ -0,0 +1,57 @@
+package DataFlow;
+
+public class Filter_CheckKeyword extends Filter {
+
+	//Class Variables & Objects
+	private static String[] keywords;
+	private static String title;
+	private static boolean isWordToIgnore;
+	
+	Filter_CheckKeyword(Pipe inPipe) {
+		super(inPipe);
+	}
+	
+	@Override
+	public void processData() {
+		
+		keywords = pipe.getArrValue();
+		title = pipe.getStrValue();
+		
+		String extractedKeyword = extractKeyword (title);
+		isWordToIgnore = checkIsWordToIgnore(extractedKeyword);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Boolean getProcessedData() {
+
+		//the '!' is to reverse the output so that if its wordsToIgnore 
+		//then its not a keyword thus it will return false otherwise if 
+		// it is not a word to ignore then it is a keyword then it will 
+		//return true
+		return !isWordToIgnore; 
+	}
+	
+	private String extractKeyword (String inTitle) {
+		
+		String extractedKeyword = "";
+		
+		extractedKeyword = inTitle.substring(0, inTitle.indexOf(' '));
+		return extractedKeyword;
+	}
+	
+	private boolean checkIsWordToIgnore (String extractedKeyword) {
+		
+		//This is implemented for user friendliness non-function feature.
+		String lowerCaseExKeyword = extractedKeyword.toLowerCase();
+		
+		for(String word:keywords) {
+			
+			if(word.toLowerCase().equals(lowerCaseExKeyword)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+};
