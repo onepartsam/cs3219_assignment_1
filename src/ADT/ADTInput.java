8db43c1a73a3ff309719556dd39b81469aad6be7
Samuel Lim Yi Jie <slyj91@gmail.com>
2015-09-01 10:42:21 +0800

    ADT: Handle input

--- src/ADT/ADTInput.java
+++ src/ADT/ADTInput.java
@@ -4,40 +4,40 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class ADTInput {
-	
+
 	private final static String ENTER_MESSAGE = "%s #%d: ";
 	private final static String TITLE = "Title";
 	private final static String IGNORED_WORD = "Ignored Word";
-	
+
 	private static Scanner scanner;
-	
-	public static LinkedList<String> inputWordsToIgnore () {
-		
+
+	public static LinkedList<String> inputWordsToIgnore() {
+
 		LinkedList<String> ignoredWords = inputLines(IGNORED_WORD);
 		return ignoredWords;
 	}
 
 	public static LinkedList<String> inputTitles() {
-		
+
 		LinkedList<String> titles = inputLines(TITLE);
 		return titles;
 	}
-	
-	private static LinkedList<String> inputLines (String keyword) {
-		
-		LinkedList<String> lines = new LinkedList<String> ();
+
+	private static LinkedList<String> inputLines(String keyword) {
+
+		LinkedList<String> lines = new LinkedList<String>();
 		String line = "";
 		int index = 1;
-		
-		scanner = new Scanner (System.in);
-		
+
+		scanner = new Scanner(System.in);
+
 		while (true) {
-			
+
 			System.out.printf(ENTER_MESSAGE, keyword, index);
 			line = scanner.nextLine();
-			
+
 			line = stripSpaces(line);
-			
+
 			if (line.equals("<exit>")) {
 				break;
 			} else {
@@ -45,12 +45,12 @@ public class ADTInput {
 				index++;
 			}
 		}
-		
+
 		return lines;
 	}
 
 	private static String stripSpaces(String line) {
-		
+
 		return line.replaceAll("\\s+", " ").trim();
 	}
 }
